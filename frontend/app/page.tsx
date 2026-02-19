4831d7dfe22500893f7f6ef8898333f50c6bf88e
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-02-19 18:08:09 +0200

    Change text back

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          sveiki draugai
+          sveiki grybai
         </div>
     </div>
   );
