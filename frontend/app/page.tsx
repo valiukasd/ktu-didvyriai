1ccbc563a025f990e5f22e792411ad5bdaf0b56a
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-02-19 18:01:27 +0200

    Change initialization text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,13 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-    
-       
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          sveiki grybai ktu
+          sveiki draugai
         </div>
-       
-    
     </div>
   );
 }
