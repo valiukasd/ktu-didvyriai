471ec7ef996372c94ffb41083ec4a723d01c59c4
Mikalojus Švėgžda <mikalojus5@gmail.com>
2026-04-01 16:19:15 +0000

    DevContainer_problem_solving

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           sveiki grybai
+
+          <h1>Tegyvuoja proletaro revoliucija!</h1>
         </div>
     </div>
   );
