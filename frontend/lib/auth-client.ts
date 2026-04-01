13ecdf765cabc9c305e53a3483a889fb3a9887f2
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-04-01 22:17:28 +0300

    Start adding postgres with better auth

--- frontend/lib/auth-client.ts
+++ frontend/lib/auth-client.ts
@@ -1,4 +1,6 @@
-import { createAuthClient } from "better-auth/react"
+import { nextCookies } from "better-auth/next-js";
+import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    plugins: [nextCookies()],
 })
--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,12 +1,15 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/client";
+
 export const auth = betterAuth({
+  plugins: [nextCookies()],
+   database: drizzleAdapter(db, {
+        provider: "pg",
+    }),
+    usePlural: true,
   emailAndPassword: { 
     enabled: true, 
-  }, 
-  socialProviders: { 
-    github: { 
-      clientId: process.env.GITHUB_CLIENT_ID as string, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-    }, 
-  }, 
+  }
 });
