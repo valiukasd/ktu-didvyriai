e18454eae0e3455b821bda1991cbb037988fd1d6
Mikalojus Švėgžda <mikalojus5@gmail.com>
2026-04-01 16:38:03 +0000

    textfix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           sveiki grybai
 
-          <h1>Tegyvuoja proletaro revoliucija!</h1>
+          sveiki draugai
         </div>
     </div>
   );
