fa2fc403edb3ca4459dfa5df53e3e8971a0d1764
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-04-02 09:46:04 +0300

    Update page content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,10 +6,7 @@ export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center font-sans ">
 			<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-				sveiki grybai
-				<Button variant="outline">Get Started</Button>
 				<Link href="/create-events">Create event</Link>
-				sveiki draugai
 			</div>
 		</div>
 	);
