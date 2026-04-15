bca41cb3f1ee7bd0032b886392cb7bdd3dc4efdd
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-04-15 16:55:51 +0000

    Fix event creation

--- frontend/lib/actions/events.ts
+++ frontend/lib/actions/events.ts
@@ -1,13 +1,16 @@
 "use server";
 
 import { parse, type InferOutput } from "valibot";
-import { authClient } from "../auth/client";
 import { EventSchema } from "../schema";
 import { db } from "@/db/client";
 import { events } from "@/db/schema";
+import { auth } from "../auth/config";
+import { headers } from "next/headers";
 
 export async function createEvent(payload: InferOutput<typeof EventSchema>) {
-	const { data: session } = await authClient.getSession();
+	const session = await auth.api.getSession({
+        headers: await headers()
+    });
 
 	if (!session) {
 		throw new Error("Unauthorized"); // Zmogus neprisijunges
