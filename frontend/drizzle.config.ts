df20d8be72fd76ec7cb82617fc35abad4e478ddb
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-05-20 16:27:07 +0000

    Rework migrations to be done with CLI

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
 	out: "./migrations",
 	schema: "./db/schema/index.ts",
 	dialect: "postgresql",
+	dbCredentials: {
+		url: process.env.DATABASE_URL as string
+	}
 });
